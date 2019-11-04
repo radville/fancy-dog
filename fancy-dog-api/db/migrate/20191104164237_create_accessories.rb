ace4a7f1bd27c9530703530930bcbcf99e008580
radville <laura.radville@gmail.com>
2019-11-04 12:52:20 -0500

    formatted home page

--- fancy-dog-api/db/migrate/20191104164237_create_accessories.rb
+++ fancy-dog-api/db/migrate/20191104164237_create_accessories.rb
@@ -2,7 +2,7 @@ class CreateAccessories < ActiveRecord::Migration[6.0]
   def change
     create_table :accessories do |t|
       t.string :name
-      t.string :url
+      t.string :src
       t.belongs_to :dog, null: false, foreign_key: true
 
       t.timestamps
