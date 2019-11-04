b2f374dd9aae23da2a16727e27a1ca255cd647b9
radville <laura.radville@gmail.com>
2019-11-04 18:52:09 -0500

    accessories can stack

--- fancy-dog-frontend/javascripts/index.js
+++ fancy-dog-frontend/javascripts/index.js
@@ -13,5 +13,6 @@ function allowDrop(ev) {
   function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    console.log(ev.target )
+    document.getElementById("accessory-selected").appendChild(document.getElementById(data));
   }
