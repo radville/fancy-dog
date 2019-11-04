6332d4ea4b222cd3e486a780558972f73af57f6e
radville <laura.radville@gmail.com>
2019-11-04 17:01:23 -0500

    added draggable accessories to main page

--- fancy-dog-frontend/javascripts/index.js
+++ fancy-dog-frontend/javascripts/index.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-}
+})
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
