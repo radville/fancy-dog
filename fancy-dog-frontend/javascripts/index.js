b481c00278511a2b5220ac3f297bb265f18f53e9
radville <laura.radville@gmail.com>
2019-11-05 08:36:14 -0500

    can put multiple accesssories on and put them back in accessory container

--- fancy-dog-frontend/javascripts/index.js
+++ fancy-dog-frontend/javascripts/index.js
@@ -13,6 +13,10 @@ function allowDrop(ev) {
   function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    console.log(ev.target )
-    document.getElementById("accessory-selected").appendChild(document.getElementById(data));
+    console.log(ev.target.parentElement.id )
+    if (ev.target.parentElement.id === "dog-form" || ev.target.parentElement.id === "accessory-selected" ) {
+        document.getElementById("accessory-selected").appendChild(document.getElementById(data));
+    } else {
+        document.getElementById("accessory").appendChild(document.getElementById(data));
+    }
   }
