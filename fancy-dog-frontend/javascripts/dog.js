686c6f5c9a9ecbb3a61346ebe94faaae91dc3643
radville <laura.radville@gmail.com>
2019-11-07 09:43:20 -0500

    fixed dog hall of fame grid

--- fancy-dog-frontend/javascripts/dog.js
+++ fancy-dog-frontend/javascripts/dog.js
@@ -14,11 +14,10 @@ class Dog {
         return `<div class="grid-item" dog-id=${this.id}>
                 <h3>Name: ${this.name}</h3>
                 <button class="delete-dog-button">Delete Dog</button>
-
                 <img class="saved-dog" src="./images/boston-terrier.png" alt="Boston Terrier">
                 ${this.accessories.map(function(accessory) { 
                     return "<img class='saved-accessory' src=" + accessory.src + ">"
-                }).join("")}
+                }).join("")}<br><br>
                 </div>`
       }
 }
